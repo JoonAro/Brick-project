e60bbd6b9faf1294443310fb1426d61466d80633
JerryUusis <jerry.uusitalo@gmail.com>
2023-11-23 11:46:46 +0200

    Add useStates and function for hamburger menu toggling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,41 @@
 import "./App.css";
+import { useState } from "react";
 import Header from './components/Header'
 import Footer from './components/Footer';
 
 function App() {
+  const [menuClass, setMenuClass] = useState("menu hidden")
+  const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setMenuClass("menu visible")
+      setIsMenuClicked(true)
+    }
+    else {
+      setMenuClass("menu hidden")
+      setIsMenuClicked(false)
+    }
+  }
+
   return (
-  <>
-    <Header />
-    <Footer />
-  </>
+
+    <>
+      <Header updateMenu={updateMenu}
+      />
+      <main>
+        <nav className={`${menuClass}`}>
+          <ul>
+            <li>Home</li>
+            <li>Categories</li>
+            <li>Black Tea</li>
+            <li>Green Tea</li>
+          </ul>
+        </nav>
+      </main>
+
+      <Footer />
+    </>
   )
 }
 
