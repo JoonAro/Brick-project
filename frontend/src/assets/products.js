d01385377916939cfd5887532e7e53b921820e22
JerryUusis <jerry.uusitalo@gmail.com>
2023-12-07 10:49:50 +0200

    Create placeholder file for products on the site

---
+++ frontend/src/assets/products.js
@@ -0,0 +1,54 @@
+export const blackTea = [
+    { name: 'Earl Grey' },
+    { name: 'English Breakfast' },
+    { name: 'Darjeeling' },
+    { name: 'Assam' },
+    { name: 'Ceylon' },
+    { name: 'Keemun' },
+  ];
+  
+  export const greenTea = [
+    { name: 'Sencha' },
+    { name: 'Dragon Well' },
+    { name: 'Matcha' },
+    { name: 'Gunpowder' },
+    { name: 'Jasmine Dragon Pearl' },
+    { name: 'Hojicha' },
+  ];
+  
+  export const jasminTea = [
+    { name: 'Jasmine Blossom' },
+    { name: 'Jasmine Silver Needle' },
+    { name: 'Jasmine Phoenix Pearls' },
+    { name: 'Jasmine Green' },
+    { name: 'Jasmine Oolong' },
+    { name: 'Jasmine Black' },
+  ];
+  
+  export const whiteTea = [
+    { name: 'Silver Needle' },
+    { name: 'White Peony' },
+    { name: 'Bai Mudan' },
+    { name: 'Shou Mei' },
+    { name: 'Gong Mei' },
+    { name: 'White Monkey Paw' },
+  ];
+  
+  export const herbalTea = [
+    { name: 'Chamomile' },
+    { name: 'Peppermint' },
+    { name: 'Lemon Balm' },
+    { name: 'Hibiscus' },
+    { name: 'Lavender' },
+    { name: 'Rooibos' },
+  ];
+  
+  export const specialTea = [
+    { name: 'Oolong Supreme' },
+    { name: 'Pu-erh Imperial' },
+    { name: 'Blooming Tea' },
+    { name: 'Golden Monkey' },
+    { name: 'Yunnan Gold' },
+    { name: 'Kenyan Purple Tea' },
+  ];
+  
